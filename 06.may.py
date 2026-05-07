920bdceaea1e7ef54f823ed19741190482d0613f
khushboo1988 <khushboosatyarthi@gmail.com>
2026-05-07 10:31:08 +0530

    new file prectic

---
+++ 06.may.py
@@ -0,0 +1,11 @@
+# wap to the last digit of and number is 456735
+num=456735
+last digit=num%10 = = 5
+print(456735%10)
+
+print(256005%100)
+
+num1=3
+num2=9
+num3=12
+print()
---
+++ 5.may.py
@@ -0,0 +1,33 @@
+s1="this is python for devops"
+find="this"
+print(find in s1)
+
+
+
+email="iqindia123@gmail.com"
+find="@gmail.com" 
+print(find in email)
+
+
+# num1="4567"
+# print("4" in num1)
+
+email="iqindia123@yahoo.com"
+find=".com"
+print(find in email)
+
+email="iqindia123@gmail.com"
+find="3"
+
+
+email="iqindia123@gmail.com"
+find="12" 
+print(find in email)
+
+
+email="iqindia123@gmail.com"
+find ="gmail.com"
+print(find in email)
+
+
+
